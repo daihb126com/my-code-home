b4c05f5fa7f41b0a10239c87b03e12a4b121660a
daihb <dai_hb@126.com>
2017-03-30 00:27:30 +0800

    Then

--- src/Main/Then.java
+++ src/Main/Then.java
@@ -1,5 +1,6 @@
+package Main;
 public class Then {
-public static void main(String[] args) {
-  System.out.println(" Then what shall we do ? ");
-}
+	public static void main(String[] args) {
+	  System.out.println(" Then what shall we do ? ");
+	}
 }
