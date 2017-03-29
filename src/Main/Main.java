452f580413bbf5983194c36cb5c9561a6080fe96
daihb <dai_hb@126.com>
2017-03-30 00:43:38 +0800

    commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("my first git project.");
-		System.out.println("I've created a new version!");
+		System.out.println("my third git project.");
 	}
 }
